1328ccf3ee334ae3f41cf34edacbe6a7e706148c
Mateo <tosellomateo0@gmail.com>
2023-10-03 22:32:10 -0300

    Frontend 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'; 
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoriasComponent } from './modules/categorias/categorias.component';
+import { ProductosComponent } from './modules/productos/productos.component';
+import { HomeComponent } from './modules/home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'categorias', component:CategoriasComponent},
+  {path:'productos', component: ProductosComponent}
+]
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriasComponent,
+    ProductosComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CategoriasComponent } from '../categorias/categorias.component';
+import { ProductosComponent } from '../productos/productos.component';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  
+}
